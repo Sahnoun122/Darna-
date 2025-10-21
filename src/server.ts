d15e13beeca5a37519e0c53ce3e48e70ab825b71
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-21 15:51:19 +0100

    chore: install and configure prettier

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -8,22 +8,20 @@ const app = express();
 
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/darna";
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/darna';
 
 mongoose
 	.connect(MONGO_URI)
-	.then(() => console.log("✅ Connecté à MongoDB"))
-	.catch((err) => console.error("❌ Erreur de connexion MongoDB:", err));
+	.then(() => console.log('Connecté à   MongoDB'))
+	.catch((err) => console.error('Erreur de connexion MongoDB:', err));
 
-// Route de test
-app.get("/", (req, res) => {
-	res.json({ message: "🚀 API fonctionnelle avec Express + TypeScript + ES6" });
+app.get('/', (req, res) => {
+	res.json({ message: '🚀 API fonctionnelle avec Express + TypeScript + ES6' });
 });
 
-// Définir le port
 const PORT = process.env.PORT || 8000;
 
 // Démarrer le serveur
 app.listen(PORT, () => {
-	console.log(`🌍 Serveur démarré sur http://localhost:${PORT}`);
+	console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
