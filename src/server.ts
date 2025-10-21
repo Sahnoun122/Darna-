51d94fd7b09393a2ee56fc08fe3e848e64279202
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-21 12:03:52 +0100

    feat: setup the server.ts

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/darna";
+
+mongoose
+	.connect(MONGO_URI)
+	.then(() => console.log("✅ Connecté à MongoDB"))
+	.catch((err) => console.error("❌ Erreur de connexion MongoDB:", err));
+
+// Route de test
+app.get("/", (req, res) => {
+	res.json({ message: "🚀 API fonctionnelle avec Express + TypeScript + ES6" });
+});
+
+// Définir le port
+const PORT = process.env.PORT || 8000;
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+	console.log(`🌍 Serveur démarré sur http://localhost:${PORT}`);
+});
