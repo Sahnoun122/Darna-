75cf8f1e65138edbd2ec594f2868b94500e87732
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-24 15:41:47 +0100

    feat: create the property controller

---
+++ src/controllers/property.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import { PropertyService } from '../services/property.service.js';
+
+const propertyService = new PropertyService();
+
+export class PropertyController {}
