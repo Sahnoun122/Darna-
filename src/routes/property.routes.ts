676f29437fb3e7ae36def124ca436e94e25d5691
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-24 15:27:24 +0100

    feat(routes): add PUT /properties/:id route

--- src/routes/property.routes.ts
+++ src/routes/property.routes.ts
@@ -8,4 +8,5 @@ const controller = new PropertyController();
 router.post('/', authenticate, controller.create.bind(controller));
 router.get('/', authenticate, controller.getAll.bind(controller));
 router.get('/:id', authenticate, controller.getById.bind(controller));
+router.put('/:id', authenticate, controller.update.bind(controller));
 export default router;
