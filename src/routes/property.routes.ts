c62e6e1c79c449afe7aa140d00f82d29efbad37d
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-24 15:27:51 +0100

    feat(routes): add DELETE /properties/:id route

--- src/routes/property.routes.ts
+++ src/routes/property.routes.ts
@@ -9,4 +9,6 @@ router.post('/', authenticate, controller.create.bind(controller));
 router.get('/', authenticate, controller.getAll.bind(controller));
 router.get('/:id', authenticate, controller.getById.bind(controller));
 router.put('/:id', authenticate, controller.update.bind(controller));
+router.delete('/:id', authenticate, controller.delete.bind(controller));
+
 export default router;
